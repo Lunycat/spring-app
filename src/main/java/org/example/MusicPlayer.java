dab896d69716528df1e667ce552d1c8735727122
Artem <lunycat@mail.ru>
2025-02-01 21:09:56 +0300

    add set DI and add properties

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -3,12 +3,40 @@ package org.example;
 public class MusicPlayer {
 
     private Music music;
+    private String name;
+    private int volume;
 
     public MusicPlayer(Music music) {
         this.music = music;
     }
 
+    public MusicPlayer() { }
+
     public void playMusic() {
         System.out.println("Playing: " + music.getSong());
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
     }
 }
